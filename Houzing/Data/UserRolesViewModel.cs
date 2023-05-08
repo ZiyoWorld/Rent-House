bd0ae7c37766a93c2dee3636404050dd0fcc7070
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2023-05-08 13:12:53 +0300

    navabar-login

---
+++ Houzing/Data/UserRolesViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Houzing.Data
+{
+    public class UserRolesViewModel
+    { 
+        public string? UserId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public IEnumerable<string>? Roles { get; set; }
+    }
+}
