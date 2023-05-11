80c804bb7beaf6f19a6cf42ffc4a564e2f739484
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2023-05-11 20:01:53 +0300

    change carousel

--- Houzing/wwwroot/js/site.js
+++ Houzing/wwwroot/js/site.js
@@ -3,6 +3,8 @@
 
 // Write your JavaScript code.
 
+
+
 const navAnim = () => {
     const burger = document.querySelector(".burger");
     const navJs = document.querySelector(".nav-js");
@@ -14,14 +16,16 @@ const navAnim = () => {
     burger.addEventListener("click", () => {
         navJs.classList.add("nav-active");
         nav.classList.add("nav-links-active");
-        burger.style.display = "none";
-        closeX.style.display = "flex";
+        burger.classList.add("close-burger");
+        closeX.classList.add("active-burger");
     });
     closeX.addEventListener("click", () => {
-        burger.style.display = "block";
-        closeX.style.display = "none";
+        burger.classList.remove("close-burger");
+        console.log("Salom")
+        closeX.classList.remove("active-burger");
         navJs.classList.remove("nav-active");
         nav.classList.remove("nav-links-active");
     });
+
    
 };
