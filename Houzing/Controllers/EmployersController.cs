4a0d33b0f35f4221d7606bf4d6711a401e5610f3
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2023-05-30 04:38:37 +0300

    apaerment index finished

--- Houzing/Controllers/EmployersController.cs
+++ Houzing/Controllers/EmployersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Houzing.Data;
 using Houzing.Data.Houses;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Houzing.Controllers
 {
@@ -18,6 +19,7 @@ namespace Houzing.Controllers
             _context = context;
         }
 
+        [Authorize(Roles = "Admin, Employer")]
         // GET: Employers
         public async Task<IActionResult> Index()
         {
@@ -135,7 +137,7 @@ namespace Houzing.Controllers
 
             return View(employer);
         }
-
+        [Authorize(Roles = "Admin")]
         // POST: Employers/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
---
+++ Houzing/Models/HouseEditItemVModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Houzing.Models
+{
+    public class HouseEditItemVModel : HouseItemModel
+    {
+        public string? ExistingImage1 { get; set; }
+        public string? ExistingImage2 { get; set; }
+        public string? ExistingImage3 { get; set; }
+    }
+}
